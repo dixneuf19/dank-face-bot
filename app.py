21f745ecabf04809ddf8fc573c06ffe7d3e1edba
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2018-09-20 23:45:51 +0200

    try to respond to the user

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from datetime import datetime
+import requests
 app = Flask(__name__)
 
 updates_from_telegram = []
+
+TOKEN = "630049189:AAGuWWg064yK7kShF8_oAg0quEb9GccZtFI"
 
 @app.route('/')
 def homepage():
@@ -19,9 +22,22 @@ def webhook():
     if request.method == 'GET':
         return "<h5>Il y a eu " + str(len(updates_from_telegram)) + " updates telegram</h5> <tr>" + str(updates_from_telegram)
     elif request.method == 'POST':
-        body = request.get_json()
-        updates_from_telegram.append(body)
+        updates = request.get_json()
+        updates_from_telegram.append(updates)
+
+
         return saved
+
+def respondToMessage(update):
+    if !(update["message"]["from"]["is_bot"]):
+        chat_id = update["chat"]["id"]
+        prenom = update["message"]["from"]["first_name"]
+
+        text = "Oh! Hi " + prenom
+
+        url = """https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}""".format(token=TOKEN, chat_id=chat_id, text=text)
+        r = requests.get(url)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
