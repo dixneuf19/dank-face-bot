6eb89b3513d8ee5c48ac1fa5ad8788384210bf00
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2018-09-21 00:16:51 +0200

    use an existing telegram bot

--- app.py
+++ app.py
@@ -1,45 +1,73 @@
-from flask import Flask, request
-from datetime import datetime
-import requests
-app = Flask(__name__)
+"""Simple Bot to reply to Telegram messages.
+This program is dedicated to the public domain under the CC0 license.
+This Bot uses the Updater class to handle the bot.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
 
-updates_from_telegram = []
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
 TOKEN = "token"
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
-@app.route('/telegram_webhook', methods=['GET', 'POST'])
-def webhook():
-    if request.method == 'GET':
-        return "<h5>Il y a eu " + str(len(updates_from_telegram)) + " updates telegram</h5> <tr>" + str(updates_from_telegram)
-    elif request.method == 'POST':
-        updates = request.get_json()
-        updates_from_telegram.append(updates)
-        for u in updates:
-            respondToMessage(update)
+logger = logging.getLogger(__name__)
 
 
-        return saved
+PORT = int(os.environ.get('PORT', '8443'))
+TOKEN = "token"
 
-def respondToMessage(update):
-    if not(update["message"]["from"]["is_bot"]):
-        chat_id = update["chat"]["id"]
-        prenom = update["message"]["from"]["first_name"]
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-        text = "Oh! Hi " + prenom
 
-        url = """https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}""".format(token=TOKEN, chat_id=chat_id, text=text)
-        r = requests.get(url)
+def help(bot, update):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(bot, update):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Start the bot."""
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+    updater.bot.set_webhook("https://dank-face-bot.herokuapp.com/" + TOKEN)
+    updater.idle()
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+    main()
