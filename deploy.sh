95902e53f6ecd8ce8efa5cd261f44cee3f4db9f6
Julen Dixneuf <julen.dixneuf@student.ecp.fr>
2018-12-25 00:41:30 +0100

    fix deployment

--- deploy.sh
+++ deploy.sh
@@ -4,14 +4,11 @@ set -e
 
 docker build -t gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT} .
 
-gcloud --quiet config set project $PROJECT_NAME_PRD
-gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
-
-gcloud docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
+docker push gcr.io/${PROJECT_NAME_PRD}/${DOCKER_IMAGE_NAME}:${TRAVIS_COMMIT}
 
 envsubst < kubernetes.template.yml > kubernetes.yml
 
+kubectl config view
+kubectl config current-context
 kubectl apply -f kubernetes.yml
 
